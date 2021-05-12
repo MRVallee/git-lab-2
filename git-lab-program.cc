95cdd1a5ab47f6204a9401854b430ee5bd626584
MRVallee <mv836315@ohio.edu>
2021-05-12 14:44:09 -0400

    Added test code.  Also added the .vs directory to the ignore list as I am using Visual Studio

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Michael Vallee
+ *        Date: 5/12/21
+ * Description: Initial Test Program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const* argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
